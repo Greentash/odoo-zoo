e9e226168826aa0a1b6e51b4d8b270587a9e1e46
DambyGreen <pouttouro@gmail.com>
2017-12-26 13:08:40 +0000

    update models.py

--- addons/zoo_animals/models.py
+++ addons/zoo_animals/models.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields
 
+BRANCH = [('1', 'Invertebres'), ('2', 'Vertebres')]
 
 class Animal(models.Model):
     _name = 'zoo.animal'
 
-    name = fields.Char(required=True)
+    name = fields.Char(string=u"Name", required=True)
+    branch = fields.Selection(BRANCH, default='2', index=True,)
+    gender = fields.Char(string=u"Gender")
